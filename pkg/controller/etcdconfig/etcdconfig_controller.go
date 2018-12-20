eac55559e3d70d8587787c615730e37316db92df
Cesar Wong <cewong@redhat.com>
2018-12-20 18:24:41 -0500

    controller - ensure config exists, vendor in library-go

--- pkg/controller/etcdconfig/etcdconfig_controller.go
+++ pkg/controller/etcdconfig/etcdconfig_controller.go
@@ -18,23 +18,24 @@ package etcdconfig
 
 import (
 	"context"
-	"reflect"
+	"fmt"
 
 	etcdv1 "github.com/openshift/cluster-etcd-operator/pkg/apis/etcd/v1"
-	appsv1 "k8s.io/api/apps/v1"
-	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/errors"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
-	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/client-go/dynamic"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/controller"
-	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
 	"sigs.k8s.io/controller-runtime/pkg/handler"
 	"sigs.k8s.io/controller-runtime/pkg/manager"
 	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 	logf "sigs.k8s.io/controller-runtime/pkg/runtime/log"
 	"sigs.k8s.io/controller-runtime/pkg/source"
+
+	"github.com/openshift/library-go/pkg/operator/v1helpers"
+
+	"github.com/openshift/cluster-etcd-operator/pkg/controller/assets"
 )
 
 var log = logf.Log.WithName("etcd-operator")
@@ -57,23 +58,35 @@ func add(mgr manager.Manager, r reconcile.Reconciler) error {
 	if err != nil {
 		return err
 	}
-
+	if err = ensureOperatorConfig(mgr); err != nil {
+		return err
+	}
 	// Watch for changes to EtcdConfig
 	err = c.Watch(&source.Kind{Type: &etcdv1.EtcdConfig{}}, &handler.EnqueueRequestForObject{})
 	if err != nil {
 		return err
 	}
+	return nil
+}
 
-	// TODO(user): Modify this to be the types you create
-	// Uncomment watch a Deployment created by EtcdConfig - change this for objects you create
-	err = c.Watch(&source.Kind{Type: &appsv1.Deployment{}}, &handler.EnqueueRequestForOwner{
-		IsController: true,
-		OwnerType:    &etcdv1.EtcdConfig{},
-	})
+func mustAsset(name string) []byte {
+	b, err := assets.Asset(name)
+	if err != nil {
+		panic(fmt.Sprintf("Asset not found: %s. Error: %v", name, err))
+	}
+	return b
+}
+
+func ensureOperatorConfig(mgr manager.Manager) error {
+	dynamicClient, err := dynamic.NewForConfig(mgr.GetConfig())
 	if err != nil {
 		return err
 	}
-
+	v1helpers.EnsureOperatorConfigExists(
+		dynamicClient,
+		mustAsset("config/operator/config.yaml"),
+		schema.GroupVersionResource{Group: etcdv1.SchemeGroupVersion.Group, Version: "v1", Resource: "etcdconfigs"},
+	)
 	return nil
 }
 
@@ -87,11 +100,6 @@ type ReconcileEtcdConfig struct {
 
 // Reconcile reads that state of the cluster for a EtcdConfig object and makes changes based on the state read
 // and what is in the EtcdConfig.Spec
-// TODO(user): Modify this Reconcile function to implement your Controller logic.  The scaffolding writes
-// a Deployment as an example
-// Automatically generate RBAC rules to allow the Controller to read and write Deployments
-// +kubebuilder:rbac:groups=apps,resources=deployments,verbs=get;list;watch;create;update;patch;delete
-// +kubebuilder:rbac:groups=apps,resources=deployments/status,verbs=get;update;patch
 // +kubebuilder:rbac:groups=etcd.operator.openshift.io,resources=etcdconfigs,verbs=get;list;watch;create;update;patch;delete
 // +kubebuilder:rbac:groups=etcd.operator.openshift.io,resources=etcdconfigs/status,verbs=get;update;patch
 func (r *ReconcileEtcdConfig) Reconcile(request reconcile.Request) (reconcile.Result, error) {
@@ -106,59 +114,6 @@ func (r *ReconcileEtcdConfig) Reconcile(request reconcile.Request) (reconcile.Re
 		}
 		// Error reading the object - requeue the request.
 		return reconcile.Result{}, err
-	}
-
-	// TODO(user): Change this to be the object type created by your controller
-	// Define the desired Deployment object
-	deploy := &appsv1.Deployment{
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      instance.Name + "-deployment",
-			Namespace: instance.Namespace,
-		},
-		Spec: appsv1.DeploymentSpec{
-			Selector: &metav1.LabelSelector{
-				MatchLabels: map[string]string{"deployment": instance.Name + "-deployment"},
-			},
-			Template: corev1.PodTemplateSpec{
-				ObjectMeta: metav1.ObjectMeta{Labels: map[string]string{"deployment": instance.Name + "-deployment"}},
-				Spec: corev1.PodSpec{
-					Containers: []corev1.Container{
-						{
-							Name:  "nginx",
-							Image: "nginx",
-						},
-					},
-				},
-			},
-		},
-	}
-	if err := controllerutil.SetControllerReference(instance, deploy, r.scheme); err != nil {
-		return reconcile.Result{}, err
-	}
-
-	// TODO(user): Change this for the object type created by your controller
-	// Check if the Deployment already exists
-	found := &appsv1.Deployment{}
-	err = r.Get(context.TODO(), types.NamespacedName{Name: deploy.Name, Namespace: deploy.Namespace}, found)
-	if err != nil && errors.IsNotFound(err) {
-		log.Info("Creating Deployment", "namespace", deploy.Namespace, "name", deploy.Name)
-		err = r.Create(context.TODO(), deploy)
-		if err != nil {
-			return reconcile.Result{}, err
-		}
-	} else if err != nil {
-		return reconcile.Result{}, err
-	}
-
-	// TODO(user): Change this for the object type created by your controller
-	// Update the found object and write the result back if there are any changes
-	if !reflect.DeepEqual(deploy.Spec, found.Spec) {
-		found.Spec = deploy.Spec
-		log.Info("Updating Deployment", "namespace", deploy.Namespace, "name", deploy.Name)
-		err = r.Update(context.TODO(), found)
-		if err != nil {
-			return reconcile.Result{}, err
-		}
 	}
 	return reconcile.Result{}, nil
 }
